7125cdda0a8242ed210d24ef675053c868e97ae4
douglaslima-pro <douglaslima-pro@outlook.com>
2024-07-11 20:47:56 -0300

    primeiro commit

---
+++ src/main/java/edu/douglaslima/santander/api/SantanderApirestNaNuvemComRailwayApplication.java
@@ -0,0 +1,13 @@
+package edu.douglaslima.santander.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderApirestNaNuvemComRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderApirestNaNuvemComRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/douglaslima/santander/api/SantanderApirestNaNuvemComRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.douglaslima.santander.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApirestNaNuvemComRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
